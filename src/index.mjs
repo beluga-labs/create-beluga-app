18cef3ef065c7a80a5fe0285409290c4ccc814c9
beluga-digital <d.baumert96@gmail.com>
2025-02-25 22:49:30 +0100

    fix module error

--- src/index.mjs
+++ src/index.mjs
@@ -32,7 +32,7 @@ figlet.text(
 );
 
 program
-  .version("0.2.0")
+  .version("0.2.1")
   .description("Create a new beluga stack app")
   .argument("<name>", "Project name")
   .action(async (name) => {
